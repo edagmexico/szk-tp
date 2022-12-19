0b91507354a97ba34f2801dd4a8ea7181b2d4aaf
Felipe Brubeck <felipe.brubeck@gmail.com>
2022-12-19 14:23:20 -0600

    Fix merge conflict 2

--- readme.py
+++ readme.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-<<<<<<< HEAD
-        codes = "ABCDEFGHIJKLMNOPSDGANBSDGIUlguksdfQRSTUVWXYZ"
-=======
         codes = "holamund12345IJKLMNOPQRSTUVWXYZ"
->>>>>>> c146368574ab5c76b77dabf588da2db835fadda2
         res   = ""
          do
             n    = n - 1
