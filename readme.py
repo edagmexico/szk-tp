d6b144eac9df99798314b9bbfa8f9816f5368238
edagmexico <arun.prakash.chavan@edag.com>
2022-12-19 22:06:56 -0600

    Update readme.py
    Final solution passed all test cases

--- readme.py
+++ readme.py
@@ -1,5 +1,4 @@
 
-Hola mundo
 class Solution(object):
     def convertToTitle(self, columnNumber):
         """
@@ -7,6 +6,7 @@ class Solution(object):
         :rtype: str
         """
         result = ""
+        result_list=[]
         equivalence_dict={}
         alphabet = list(string.ascii_uppercase)
         print("alphabet: {}".format(alphabet))
@@ -17,11 +17,33 @@ class Solution(object):
         print("columnNumber: {}".format(columnNumber))
         div=columnNumber
         temp=columnNumber % 26
-        print("temp: {}".format(temp))
+        div=div/26
+        print("temp: {}, result {}, div {}".format(temp, result_list, div))
+        if (div>0 and div <= 26):
+            result_list.append(equivalence_dict[temp-1])
+            result_list.append(equivalence_dict[div-1])
+            #result=equivalence_dict[div-1]+equivalence_dict[temp-1]
+        else:
+            result_list.append(equivalence_dict[temp-1])
+            #result=equivalence_dict[temp-1]
+        
         counter=0
-        while temp != 0:
-            div=div / 26
+        while div > 26:
             temp=div % 26
-            print("counter {}, temp {}, div {}".format(counter, temp, div))
+            div=div/26
+            if (div>0 and div <= 26):
+                result_list.append(equivalence_dict[temp-1])
+                result_list.append(equivalence_dict[div-1])
+                #result=equivalence_dict[div-1]+equivalence_dict[temp-1]
+            else:
+                result_list.append(equivalence_dict[temp-1])
+            #if (div>0 and div <= 26):
+            #    result+=equivalence_dict[div-1]+equivalence_dict[temp-1]
+            #else:
+            #    result+=equivalence_dict[temp-1]
+            
+            print("counter {}, temp {}, div {}, result {}".format(counter, temp, div, result_list))
             counter+=1
-        return "AA"
+        #result =  result_list.reverse()
+        result = result_list[::-1]
+        return ''.join(result)
